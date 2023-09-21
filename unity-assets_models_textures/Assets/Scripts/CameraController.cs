815f097cdb11b27e852360ad6092fd5389bf4b84
unknown <5013@holbertonschool.com>
2023-09-21 13:36:45 +0200

    creat Canvas timer

--- unity-assets_models_textures/Assets/Scripts/CameraController.cs
+++ unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -1,1 +1,37 @@
-    
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // Le transform du joueur
+    public Vector3 offset; // L'écart entre la caméra et le joueur
+    public float smoothSpeed = 0.125f; // Fluidité du mouvement de la caméra
+
+    // Nouvelles variables pour la rotation de la caméra
+    public float rotationSpeed = 2.0f; // Vitesse de rotation de la caméra
+    private float rotationX = 0.0f;
+    private float rotationY = 0.0f;
+
+    void LateUpdate()
+    {
+        // Mouvement de la caméra
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        // Regarder le joueur
+        transform.LookAt(target);
+
+        // Rotation de la caméra avec le clic droit de la souris
+        if (Input.GetMouseButton(1)) // Le clic droit est enfoncé
+        {
+            rotationX -= Input.GetAxis("Mouse Y") * rotationSpeed;
+            rotationY += Input.GetAxis("Mouse X") * rotationSpeed;
+
+            // Limiter la rotation verticale pour éviter le retournement de la caméra
+            rotationX = Mathf.Clamp(rotationX, -90, 90);
+
+            // Appliquer la rotation à la caméra
+            transform.rotation = Quaternion.Euler(rotationX, rotationY, 0);
+        }
+    }
+}
--- unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterControllerMovement : MonoBehaviour
 {
@@ -10,6 +11,7 @@ public class CharacterControllerMovement : MonoBehaviour
     public float playerSpeed = 5.0f;
     public float jumpHeight = 1.0f;
     public float gravityValue = -9.81f;
+    public Transform player;
 
     private void Start()
     {
@@ -23,7 +25,7 @@ public class CharacterControllerMovement : MonoBehaviour
         if (groundedPlayer && playerVelocity.y < -20)
         {
             playerVelocity.y = 0f;
-           
+            ;
         }
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
@@ -33,7 +35,12 @@ public class CharacterControllerMovement : MonoBehaviour
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-            Debug.Log("sauter");
+        }
+
+        if (player.position.y <= -10.0f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            player.position = new Vector3(player.position.x, player.position.y * 20.0f, player.position.z);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
